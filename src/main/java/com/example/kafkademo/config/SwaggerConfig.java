8eadef9a6b721b63f2c2d4d2f76aeaee433671eb
mj.love <mj.love@kakaoinsurecorp.com>
2024-04-25 11:04:59 +0900

    swagger 설정변경

--- src/main/java/com/example/kafkademo/config/SwaggerConfig.java
+++ src/main/java/com/example/kafkademo/config/SwaggerConfig.java
@@ -1,24 +1,32 @@
 package com.example.kafkademo.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("API Test") // API의 제목
-                .description("Let's practice Swagger UI") // API에 대한 설명
-                .version("1.0.0"); // API의 버전
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Kafka Demo")
+                .description("Kafka Demo입니다.")
+                .version("1.0")
+                .build();
     }
 }
