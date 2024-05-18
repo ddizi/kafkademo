074c269c642c3f4dab4cfff6f96f93eaab3a0cae
ddizi <ruroo@naver.com>
2024-05-18 10:08:18 +0900

    uri 수정

--- src/main/java/com/example/kafkademo/controller/CustomerController.java
+++ src/main/java/com/example/kafkademo/controller/CustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/customer")
+@RequestMapping("api/v1")
 @Tag(name = "Response Estimate", description = "Response Estimate API")
 public class CustomerController {
 
