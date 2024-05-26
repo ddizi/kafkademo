56075c1cbdbd42f649de3cdbe2f108c1f1d9acb5
ddizi <ruroo@naver.com>
2024-05-26 14:48:18 +0900

    CustomerService Test

--- src/test/java/com/example/kafkademo/service/CustomerServiceTest.java
+++ src/test/java/com/example/kafkademo/service/CustomerServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.kafkademo.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ class CustomerServiceTest {
     CustomerService customerService;
 
     @Test
+    @DisplayName("Create new customer")
     void createNewCustomer() {
         CustomerDto customerDto = CustomerDto.builder()
                 .customerName("John Doe")
@@ -21,5 +23,18 @@ class CustomerServiceTest {
                 .build();
         CustomerDto customer = customerService.createCustomer(customerDto);
         assertEquals(30, customer.getAge());
+    }
+
+    @Test
+    @DisplayName("Get customer")
+    void getCustomer() {
+        CustomerDto customerDto = CustomerDto.builder()
+                .customerName("John Doe")
+                .age(30)
+                .genderCode("1")
+                .build();
+        CustomerDto customer = customerService.createCustomer(customerDto);
+        CustomerDto customerFromDb = customerService.getCustomer(customer.getCustomerId().toString());
+        assertEquals("John Doe", customerFromDb.getCustomerName());
     }
 }
